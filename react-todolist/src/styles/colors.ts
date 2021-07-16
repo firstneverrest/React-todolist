9d4301659da3ee2e20efb2ff841ed33c2893a3c0
Chitsanupong <first30952@gmail.com>
2021-07-16 21:30:13 +0700

    add colors and set theme

---
+++ react-todolist/src/styles/colors.ts
@@ -0,0 +1,20 @@
+const colors = {
+  default: {
+    primary: '#3F51B5',
+    secondary: '#F50057',
+    error: '#F44336',
+    warning: '#FF9800',
+    info: '#2196F3',
+    success: '#4CAF50',
+  },
+
+  // customized palette
+  darkPurple: '#8675A9',
+  lightPurple: '#C3AED6',
+  darkGrey: '#858585',
+  lightGrey: '#A1A1A1',
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+export default colors;
