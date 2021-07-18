0789d525b84c0d25510ef970b0142b6c5b9f21f2
Chitsanupong <first30952@gmail.com>
2021-07-18 12:30:49 +0700

    create cookie util

---
+++ react-todolist/src/utils/cookie.ts
@@ -0,0 +1,26 @@
+export const setCookie = (cname: string, cvalue: string, exdays: number) => {
+  const date = new Date();
+  date.setTime(date.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = 'expires=' + date.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+};
+
+export const getCookie = (cname: string) => {
+  let name = cname + '=';
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+
+export const deleteCookie = (cname: string) => {
+  document.cookie =
+    'username=' + cname + '; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+};
